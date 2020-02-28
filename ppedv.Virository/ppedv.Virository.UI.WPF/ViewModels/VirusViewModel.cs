735232af6f2b72832d4be5913d2905dc6b806572
André <andrer@ppedv.de>
2020-02-28 14:34:02 +0100

    Binding (simple)

---
+++ ppedv.Virository/ppedv.Virository.UI.WPF/ViewModels/VirusViewModel.cs
@@ -0,0 +1,23 @@
+﻿using ppedv.Virository.Logic;
+using ppedv.Virository.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ppedv.Virository.UI.WPF.ViewModels
+{
+    public class VirusViewModel
+    {
+        public List<Virus> VirenListe { get; set; }
+
+        public Virus SelectedVirus { get; set; }
+
+        Core core = new Core();
+
+        public VirusViewModel()
+        {
+            VirenListe = new List<Virus>(core.Repository.GetAll<Virus>());
+        }
+
+    }
+}
