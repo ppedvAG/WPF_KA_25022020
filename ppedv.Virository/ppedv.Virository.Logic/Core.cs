1d88bab615b7ba11937db78782aadadaa5714ff4
André <andrer@ppedv.de>
2020-02-28 11:46:15 +0100

    LogicLayer / Repository Fertig

---
+++ ppedv.Virository/ppedv.Virository.Logic/Core.cs
@@ -0,0 +1,18 @@
+﻿using ppedv.Virository.Model.Contracts;
+using System;
+
+namespace ppedv.Virository.Logic
+{
+    public class Core
+    {
+        public IRepository Repository { get; private set; }
+
+        public Core(IRepository repo)
+        {
+            Repository = repo;
+        }
+
+        public Core() : this(new Data.EF.EfRepository())
+        { }
+    }
+}
---
+++ ppedv.Virository/ppedv.Virository.Model/Contracts/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ppedv.Virository.Model.Contracts
+{
+    public interface IRepository
+    {
+        T GetById<T>(int id) where T : Entity;
+        IEnumerable<T> GetAll<T>() where T : Entity;
+
+        void Add<T>(T entity) where T : Entity;
+        void Delete<T>(T entity) where T : Entity;
+        void Update<T>(T entity) where T : Entity;
+
+        int SaveChanges();
+    }
+}
